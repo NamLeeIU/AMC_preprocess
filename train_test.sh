502e442a4fd049a2561f9a19e41e0cdc0ced6abf
NamLeeIU <namlequoc2001@gmail.com>
2022-09-04 13:54:50 +0700

    Update gitignore

--- train_test.sh
+++ train_test.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
-
+eval "$(conda shell.bash hook)"
+conda activate rml
 classifier="LDA"   # choose from 'LRG' 'LDA' 'SGD' 
 cuda=true   # otherwise "False"
-
 python3 gen_subset.py
 python3 linear_trans.py
 python3 second_trans.py  --classifier $classifier  --cuda $cuda
 python3 high_trans.py  --classifier $classifier  --cuda $cuda
+bash
